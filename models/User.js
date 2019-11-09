19ddb870bc7797c44f2c4cc6bf6a0ff83bbf9aac
Garvin <gming27@gmail.com>
2019-11-08 19:23:43 -0500

    adds owned lists,  hosted and participated exchanges fetching functionality to the UserSchema and user_type

--- models/User.js
+++ models/User.js
@@ -41,4 +41,56 @@ const UserSchema = new Schema({
 
 })
 
+UserSchema.statics.addWishlist = (exchangeId, listId) => {
+  const Exchange = mongoose.model('exchanges');
+  const User =  mongoose.model('users');
+
+  return Exchange.findById(exchangeId).then(exchange => {
+      exchange.wish_list_ids.push(listId);
+      return User.owned_lists.push(listId);
+  })
+  .catch(err => res.json(err))
+}
+
+UserSchema.statics.fetchHostedExchanges = (userId) => {
+  const User = mongoose.model('users');
+
+  return User.findById(userId).then(user => {
+    if (user.hosted_exchanges) {
+      console.log(user.hosted_exchanges);
+      return user.hosted_exchanges
+    } else {
+      return ({ msg: "You have no hosted exchanges." })
+    }
+  })
+}
+
+UserSchema.statics.fetchParticipatedExchanges = (userId) => {
+  const User = mongoose.model('users');
+
+  return User.findById(userId).then(user => {
+    if (user.participated_exchanges) {
+      console.log(user.participated_exchanges);
+      return user.participated_exchanges
+    } else {
+      return ({ msg: "You have no exchanges you've participated in." })
+    }
+  })
+}
+
+UserSchema.statics.fetchOwnedLists = (userId) => {
+  const User = mongoose.model('users');
+
+  return User.findById(userId).then(user => {
+    if (user.owned_lists) {
+      console.log(user.owned_lists);
+      return user.owned_lists
+    } else {
+      return ({ msg: "You have no wishlists." })
+    }
+  })
+}
+
+
+
 module.exports = mongoose.model("users", UserSchema);
