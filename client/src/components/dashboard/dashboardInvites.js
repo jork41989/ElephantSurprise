18ba32feff822883bf6aab0ad400af5b1e572166
Garvin <gming27@gmail.com>
2019-11-13 16:08:51 -0500

    creates an updateCache method on DashboardInvites

--- client/src/components/dashboard/dashboardInvites.js
+++ client/src/components/dashboard/dashboardInvites.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Mutations from "../../graphql/mutations";
+import Queries from "../../graphql/queries";
 import {Mutation} from "react-apollo";
 const {ADD_PARTICIPANT} = Mutations;
 const {DELETE_INVITE} = Mutations;
+const {FETCH_USER} = Queries;
 
 
 class DashboardInvites extends Component{
@@ -13,6 +15,26 @@ class DashboardInvites extends Component{
     }
   }
 
+
+  updateCache(cache, { data: { pending_invites } }) {
+    let user;
+    try {
+      user = cache.readQuery({ 
+        query: FETCH_USER,
+        variables: { _id: this.props.user._id}
+       });
+    } catch (err) {
+      return;
+    }
+
+    if (user) {
+        cache.writeQuery({
+        query: FETCH_USER,
+        variables: { _id: this.props.user._id },
+        data: { pending_invites: pending_invites }
+      });
+    }
+  }
 
   render(){
     let count = 0
@@ -30,11 +52,13 @@ class DashboardInvites extends Component{
                 {invite.name}
               <p>Accept this Invite?</p>
 
-             <Mutation mutation={ADD_PARTICIPANT}>
+             <Mutation 
+              mutation={ADD_PARTICIPANT}
+              update={(cache, data) => this.updateCache(cache, data)}
+             >
                 {(addParticipant, data) => (
                   <button onClick={e=>{
                     e.preventDefault();
-                      console.log("invite", invite._id);
                     addParticipant({
                       variables: { exchange_id: invite._id, user_id: this.props.user._id }
                     });
