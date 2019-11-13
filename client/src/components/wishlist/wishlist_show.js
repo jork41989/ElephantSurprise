f2f5fae6e4c0cc439bb8e3529b02c6a29a698057
Garvin <gming27@gmail.com>
2019-11-13 12:15:30 -0500

    creates WishlistShow component that fetches user information to the component

--- client/src/components/wishlist/wishlist_show.js
+++ client/src/components/wishlist/wishlist_show.js
@@ -11,7 +11,31 @@ class WishlistShow extends Component{
 
   render(){
 
+    return (
+      <Query query={CURRENT_USER}>
+        {({ loading, error, data }) => {
 
+          return (
+            <Query query={FETCH_USER}
+              variables={{ _id: data.CurrentUserID }}
+            >
+              {({ loading, error, data }) => {
+                if (loading)
+                  return "Loading...";
+                if (error)
+                  return `Error! ${error.message}`;
+                // console.log(data)
+                return (
+                  <div className="ExchangeNacelle">
+                    <ItemsIndex user={data.user} />
+                  </div>
+                )
+              }
+              }
+            </Query>
+          )
+        }}
+      </Query>)
 
   }
 
