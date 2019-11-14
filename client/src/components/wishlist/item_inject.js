c4c1f7dece346bc4d3b1dacca9dc8f15e3efc86f
Garvin <gming27@gmail.com>
2019-11-13 20:18:45 -0500

    creates ItemInject form with ADD_ITEM mutation, requires error checking and finalizing

--- client/src/components/wishlist/item_inject.js
+++ client/src/components/wishlist/item_inject.js
@@ -36,6 +36,18 @@ class ItemInject extends Component{
     }
   }
 
+  errorTips() {
+    if (this.state.errors) {
+      return (
+        <ReactTooltip id="email" place="top" type="error" effect="solid">
+          <span>{this.state.errors[0].message}</span>
+        </ReactTooltip>
+      )
+
+    }
+  }
+
+
   render(){
 
     return(
