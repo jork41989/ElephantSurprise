c0f0e2765c704a38f15d7e290e503460175ef1a3
Garvin <gming27@gmail.com>
2019-11-13 20:18:45 -0500

    creates ItemInject form with ADD_ITEM mutation, requires error checking and finalizing

--- client/src/components/wishlist/item_inject.js
+++ client/src/components/wishlist/item_inject.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Mutations from "../../graphql/mutations";
 import { Mutation } from "react-apollo";
+import ReactTooltip from "react-tooltip";
 
 const {ADD_ITEM} = Mutations;
 
@@ -9,10 +10,11 @@ class ItemInject extends Component{
     super(props)
     this.state ={
       url: "",
-      price: ""
+      price: "",
+      errors: ""
     }
 
-
+    this.errorTips = this.errorTips.bind(this);
   }
 
   update(field) {
@@ -23,6 +25,18 @@ class ItemInject extends Component{
 
   }
 
+  errorTips() {
+    if (this.state.errors) {
+      return (
+        <ReactTooltip id="email" place="top" type="error" effect="solid">
+          <span>{this.state.errors[0].message}</span>
+        </ReactTooltip>
+      )
+
+    }
+  }
+
+
   render(){
 
     return(
@@ -30,8 +44,27 @@ class ItemInject extends Component{
         <h2>Update Your List with an Item</h2>
         <Mutation mutation={ADD_ITEM}>
           <form onSubmit={e =>{
+            e.preventDefault();
+            this.setState({errors: null});
+            newItem({
+              variables: {
+                url: this.state.url,
+                price: this.state.price,
+                owner_id: this.props.user._id,
+                wish_list_id: this.props.wishlist._id
+              }
+            })
 
           }}>
+            {this.errorTips()}
+
+            <input value={this.state.url}
+              onChange={this.update("url")}
+            ></input>
+
+            <input value={this.state.price}
+              onChange={this.update("price")}
+            ></input>
 
           </form>
         </Mutation>
